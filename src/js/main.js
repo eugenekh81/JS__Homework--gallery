cad42c9206afbe846e10fc8878b5c25980dfbd17
Vitaly Grodetsky <rogue.moore.private@gmail.com>
2024-12-01 23:31:06 +0200

    added some styles

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,7 @@
 // insert your code here
+import Notiflix from 'notiflix';
+import axios from 'axios';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+
